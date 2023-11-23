2c3eb8831782efa833348a3bb578ac08201455fe
Студент <@1309-3>
2023-11-23 13:18:13 +0400

    /

---
+++ app/src/main/java/com/PC/Component/Guru/app/optimalBuild/OptimalBuildCaseComPcComponentGuru.kt
@@ -0,0 +1,13 @@
+package com.PC.Component.Guru.app.optimalBuild
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.PC.Component.Guru.app.R
+
+
+class OptimalBuildCaseComPcComponentGuru : Fragment() {
+
+}
